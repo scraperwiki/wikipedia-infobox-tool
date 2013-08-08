e8197b020128dd0806416dcc34f770ebc8137d47
Matthew Hughes <me@matthewhughes.co.uk>
2013-08-08 11:53:48 +0100

    Added requirements.txt

--- wikipedia.py
+++ wikipedia.py
@@ -1,8 +1,15 @@
 import requests
 import json
+import scraperwiki
 
 query = 'format=json&action=query&titles=United%20Kingdom&prop=revisions&rvprop=content'
 request = requests.get('http://en.wikipedia.org/w/api.php?' + query)
 
 pageid = request.json()['query']['pages'].keys()[0]
 print request.json()['query']['pages'][pageid]['revisions'][0]['*']
+
+def main():
+    
+
+if __name__ == '__main__':
+    main()
