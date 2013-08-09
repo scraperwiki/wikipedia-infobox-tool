e22a49e814e638bf2e10cbb021ce09d50802cf74
Sean Duffy <seanduffy@live.com>
2013-08-09 10:47:33 +0000

    Refactored code to prepare for category iteration

--- wikipedia.py
+++ wikipedia.py
@@ -1,27 +1,47 @@
 import requests
 import json
 import scraperwiki
+import re
 
-query = 'format=json&action=query&titles=ScraperWiki&prop=revisions&rvprop=content'
-request = requests.get('http://en.wikipedia.org/w/api.php?' + query)
-pageid = request.json()['query']['pages'].keys()[0]
-content = request.json()['query']['pages'][pageid]['revisions'][0]['*']
+pageid = 37534696
 
-content = content.split('\n|')
+def clean_brackets(item):
+    return re.sub('(\[\[)|(\]\])', '', item)
 
-content[-1] = content[-1][:content[-1].find('\n'):]
+def clean_html(item):
+    return re.sub('<[^<]+?>', '', item)
 
-data = {}
+def scrape_infobox(pageid):
+    query = 'format=json&action=query&pageids=%s&prop=revisions&rvprop=content' % pageid
+    request = requests.get('http://en.wikipedia.org/w/api.php?' + query)
+    json_content = request.json()
+    pageid = json_content['query']['pages'].keys()[0]
+    content = json_content['query']['pages'][pageid]['revisions'][0]['*']
+    article_name = json_content['query']['pages'][pageid]['title']    
 
-for item in content[1::]:
-    pair = item[1::].split('=')
-    try:
-        field = pair[0].strip()
-        value = pair[1].strip()
-        print field, value
-        data[field] = value
-        data['id'] = 0
-    except IndexError:
-        print 'IndexError!'
+    content = content.split('\n|')
 
-scraperwiki.sql.save(['id'], data)
+    content[-1] = content[-1][:content[-1].find('\n'):]
+
+    data = {}
+
+    for item in content[1::]:
+        pair = item.split('=', 1)
+        try:
+            field = pair[0].strip()
+            value = pair[1].strip()
+            value = clean_brackets(value)
+            value = clean_html(value)
+            data[field] = value
+            data['Article Name'] = article_name
+        except IndexError:
+            print 'IndexError!'
+
+    scraperwiki.sql.save(['Article Name'], data)
+
+def main():
+    scrape_infobox(pageid)
+
+if __name__ == '__main__':
+    main()
+
