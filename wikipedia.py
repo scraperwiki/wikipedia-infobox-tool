fc0f7e99387af04a86088a64580e0bd96f3833ba
Sean Duffy <seanduffy@live.com>
2013-08-09 14:06:04 +0000

    Scraping by category is now possible

--- wikipedia.py
+++ wikipedia.py
@@ -4,6 +4,7 @@ import scraperwiki
 import re
 
 pageid = 37534696
+api_url = 'http://en.wikipedia.org/w/api.php?' 
 
 def clean_brackets(item):
     return re.sub('(\[\[)|(\]\])', '', item)
@@ -11,17 +12,25 @@ def clean_brackets(item):
 def clean_html(item):
     return re.sub('<[^<]+?>', '', item)
 
+def scrape_members(category):
+    query = 'action=query&list=categorymembers&cmtitle=%s&format=json&cmsort=timestamp&cmdir=desc' % category
+    request = requests.get(api_url + query)
+    json_content = request.json()
+    data_list = []
+    for member in json_content['query']['categorymembers']:
+        data_list.append(scrape_infobox(member['pageid']))
+    scraperwiki.sql.save(['id'], data_list)
+
 def scrape_infobox(pageid):
     query = 'format=json&action=query&pageids=%s&prop=revisions&rvprop=content' % pageid
-    request = requests.get('http://en.wikipedia.org/w/api.php?' + query)
+    request = requests.get(api_url + query)
     json_content = request.json()
     pageid = json_content['query']['pages'].keys()[0]
     content = json_content['query']['pages'][pageid]['revisions'][0]['*']
     article_name = json_content['query']['pages'][pageid]['title']    
 
+    content = content[:re.search('\n.*\}\}', content).start():]
     content = content.split('\n|')
-
-    content[-1] = content[-1][:content[-1].find('\n'):]
 
     data = {}
 
@@ -32,15 +41,18 @@ def scrape_infobox(pageid):
             value = pair[1].strip()
             value = clean_brackets(value)
             value = clean_html(value)
-            data[field] = value
-            data['Article Name'] = article_name
+            if field not in data.keys():
+                data[field] = value
+            data['id'] = pageid
+            data['article_name'] = article_name
         except IndexError:
             print 'IndexError!'
+            exit(1)
 
-    scraperwiki.sql.save(['Article Name'], data)
+    return data
 
 def main():
-    scrape_infobox(pageid)
+    scrape_members('Category:Recipients_of_the_Order_of_the_Seraphim')
 
 if __name__ == '__main__':
     main()
