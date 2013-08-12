9529862939953425e44727a84a9b51285060105b
Sean Duffy <seanduffy@live.com>
2013-08-12 15:53:13 +0000

    Categories are now recursively scraped

--- wikipedia.py
+++ wikipedia.py
@@ -1,10 +1,10 @@
-import requests
+import sys
 import json
-import scraperwiki
 import re
+import requests
+import scraperwiki
 
-pageid = 37534696
-api_url = 'http://en.wikipedia.org/w/api.php?' 
+api_url = 'http://en.wikipedia.org/w/api.php?action=query&format=json' 
 
 def clean_brackets(item):
     return re.sub('(\[\[)|(\]\])', '', item)
@@ -12,33 +12,51 @@ def clean_brackets(item):
 def clean_html(item):
     return re.sub('<[^<]+?>', '', item)
 
+def get_data_list(members):
+    data_list = []
+    for member in members:
+        if 'Category:' in member['title']:
+            scrape_members(member['title']) 
+        else:
+            print "Scraping infobox for '%s'" % member['title']
+            data = scrape_infobox(member['pageid'])
+            if data != None and len(data) > 0:
+                data_list.append(data)
+                print "Scraping done for '%s'" % member['title']
+    return data_list
+
 def scrape_members(category):
-    query = 'action=query&list=categorymembers&cmtitle=%s&format=json&cmsort=timestamp&cmdir=desc&cmlimit=max' % category
+    query = '&list=categorymembers&cmtitle=%s&cmsort=timestamp&cmdir=desc&cmlimit=max' % category
     request = requests.get(api_url + query)
     json_content = request.json()
-    data_list = []
-    for member in json_content['query']['categorymembers']:
-        print member['pageid']
-        data = scrape_infobox(member['pageid'])
-        if data != None:
-            data_list.append(data)
-    scraperwiki.sql.save(['id'], data_list)
+    data_list = get_data_list(json_content['query']['categorymembers'])
+    if len(data_list) > 0:
+        scraperwiki.sql.save(['id'], data_list)
 
 def scrape_infobox(pageid):
-    query = 'format=json&action=query&pageids=%s&prop=revisions&rvprop=content' % pageid
+    query = '&action=query&pageids=%s&prop=revisions&rvprop=content' % pageid
     request = requests.get(api_url + query)
     json_content = request.json()
     pageid = json_content['query']['pages'].keys()[0]
     content = json_content['query']['pages'][pageid]['revisions'][0]['*']
     article_name = json_content['query']['pages'][pageid]['title']    
 
-    if 'infobox' not in content.lower():
-        print 'Infobox not found for ' + article_name
+    if 'infobox' in content.lower():
+        content = content[content.lower().find('{{infobox')::]
+    elif 'taxobox' in content.lower():
+        content = content[content.lower().find('{{taxobox')::]
+    else:
+        print "Infobox not found for '%s'" % article_name
         return None
 
-    content = content[content.lower().find('{{infobox')::]
-    content = content[:re.search('\n[^\n{]*\}\}[^\n{]*\n', content).start():]
-    content = content.split('\n|')
+    infobox_end = re.search('\n[^\n{]*\}\}[^\n{]*\n', content)
+
+    if infobox_end == None:
+        print "Closing tag not found for '%s'" % article_name
+        return None
+
+    content = content[:infobox_end.start():]
+    content = re.split('\n[^|]\|', content)
 
     data = {}
 
@@ -50,8 +68,7 @@ def scrape_infobox(pageid):
                 value = pair[1].strip()
                 value = clean_brackets(value)
                 value = clean_html(value)
-                if field not in data.keys():
-                    data[field] = value
+                data[field.lower()] = value
                 data['id'] = pageid
                 data['article_name'] = article_name
             except IndexError:
@@ -61,7 +78,7 @@ def scrape_infobox(pageid):
     return data
 
 def main():
-    scrape_members('Category:Airports_in_England')
+    scrape_members(sys.argv[1])
 
 if __name__ == '__main__':
     main()
