15dc5ceed452c606735695dd91097a405c211fd3
Sean Duffy <seanduffy@live.com>
2013-08-13 15:41:33 +0000

    Added a script to count the number of pages in a category

--- page_count.py
+++ page_count.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 
+import sys
+import re
+import requests
+import scraperwiki
+
+api_url = 'http://en.wikipedia.org/w/api.php?action=query&format=json' 
+i = 0
+
+def count_members(category):
+    query = '&list=categorymembers&cmtitle=%s&cmsort=timestamp&cmdir=desc&cmlimit=max' % category
+    request = requests.get(api_url + query)
+    json_content = request.json()
+    members = json_content['query']['categorymembers']
+
+    for member in members:
+        if 'Category:' in member['title']:
+            count_members(member['title'])
+        else:
+            global i
+            i = i + 1
+
 def main():
+    category = sys.argv[1].replace(' ', '_')
+    
+    n_list = count_members('Category:' + category)
 
+    print i
 
+if __name__ == '__main__': 
+    main()
